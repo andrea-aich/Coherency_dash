591b22d12243960eab0e9867f66823d2750a717c
Andrea Ibarra Chaoul <andrea.aich@gmail.com>
2026-04-07 20:49:08 +0200

    Cleaned up a bit.

--- app.py
+++ app.py
@@ -7,21 +7,10 @@ from dash.dependencies import Input, Output
 from scipy.fftpack import fft, fftn
 from scipy.signal.windows import hann
 
-# Load data
-df = pd.read_csv('data/stockdata2.csv', index_col=0, parse_dates=True, date_format='%Y-%m-%d')
-df.index = pd.to_datetime(df['Date'])
 
 # Initialize the app
 app = dash.Dash(__name__)
 app.config.suppress_callback_exceptions = True
-
-
-def get_options(list_stocks):
-    dict_list = []
-    for i in list_stocks:
-        dict_list.append({'label': i, 'value': i})
-
-    return dict_list
 
 
 app.layout = html.Div(className='big-app-container',
@@ -160,82 +149,6 @@ app.layout = html.Div(className='big-app-container',
 @app.callback(Output('output', 'children'), Input('input', 'value'))
 def display_output(value):
     return 'You have entered {}'.format(value)
-
-
-# Callback for timeseries price
-@app.callback(Output('timeseries', 'figure'),
-              [Input('stockselector', 'value')])
-def update_timeseries(selected_dropdown_value):
-    ''' Draw traces of the feature 'value' based one the currently selected stocks '''
-    # STEP 1
-    trace = []
-    df_sub = df
-    # STEP 2
-    # Draw and append traces for each stock
-    for stock in selected_dropdown_value:
-        trace.append(go.Scatter(x=df_sub[df_sub['stock'] == stock].index,
-                                 y=df_sub[df_sub['stock'] == stock]['value'],
-                                 mode='lines',
-                                 opacity=0.7,
-                                 name=stock,
-                                 textposition='bottom center'))
-    # STEP 3
-    traces = [trace]
-    data = [val for sublist in traces for val in sublist]
-    # Define Figure
-    # STEP 4
-    figure = {'data': data,
-              'layout': go.Layout(
-                  colorway=["#5E0DAC", '#FF4F00', '#375CB1', '#FF7400', '#FFF400', '#FF0056'],
-                  template='plotly_dark',
-                  paper_bgcolor='rgba(0, 0, 0, 0)',
-                  plot_bgcolor='rgba(0, 0, 0, 0)',
-                  margin={'b': 15},
-                  hovermode='x',
-                  autosize=True,
-                  title={'text': 'Stock Prices', 'font': {'color': 'white'}, 'x': 0.5},
-                  xaxis={'range': [df_sub.index.min(), df_sub.index.max()]},
-              ),
-              }
-
-    return figure
-
-
-@app.callback(Output('change', 'figure'),
-              [Input('stockselector', 'value')])
-def update_change(selected_dropdown_value):
-    ''' Draw traces of the feature 'change' based one the currently selected stocks '''
-    trace = []
-    df_sub = df
-    # Draw and append traces for each stock
-    for stock in selected_dropdown_value:
-        trace.append(go.Scatter(x=df_sub[df_sub['stock'] == stock].index,
-                                 y=df_sub[df_sub['stock'] == stock]['change'],
-                                 mode='lines',
-                                 opacity=0.7,
-                                 name=stock,
-                                 textposition='bottom center'))
-    traces = [trace]
-    data = [val for sublist in traces for val in sublist]
-    # Define Figure
-    figure = {'data': data,
-              'layout': go.Layout(
-                  colorway=["#5E0DAC", '#FF4F00', '#375CB1', '#FF7400', '#FFF400', '#FF0056'],
-                  #template='plotly_dark',
-                  paper_bgcolor='rgba(0, 0, 0, 0)',
-                  plot_bgcolor='rgba(0, 0, 0, 0)',
-                  margin={'t': 50},
-                  height=250,
-                  hovermode='x',
-                  autosize=True,
-                  title={'text': 'Daily Change', 'font': {'color': 'white'}, 'x': 0.5},
-                  xaxis={'showticklabels': False, 'range': [df_sub.index.min(), df_sub.index.max()]},
-              ),
-              }
-
-    return figure
-
-
 
 # trying my own callback for the sliders
 @app.callback(Output('source-A', 'figure'),
